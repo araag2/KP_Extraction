664e655eae36bb325527905a12df384f30e3645b
araag2 <artur.guimas@gmail.com>
2021-08-12 18:49:38 +0100

    Got SentenceBert Embeddings working

--- models/embedrank/embedrank_model.py
+++ models/embedrank/embedrank_model.py
@@ -11,7 +11,7 @@ from models.pre_processing.pos_tagging import POS_tagger_spacy
 from models.pre_processing.pre_processing_utils import remove_punctuation, remove_whitespaces
 from models.pre_processing.document_abstraction import Document
 from datasets.process_datasets import *
-from evaluation.evaluation_tools import evaluate_kp_extraction, extract_dataset_labels, extract_res_labels
+from evaluation.evaluation_tools import evaluate_candidate_extraction, evaluate_kp_extraction, extract_dataset_labels, extract_res_labels
 
 class EmbedRank(BaseKPModel):
     """
@@ -33,41 +33,17 @@ class EmbedRank(BaseKPModel):
         doc = remove_punctuation(doc)
         return remove_whitespaces(doc)[1:]
 
-    def top_n_candidates(self, doc : Document = None, candidate_list : List[str] = [], top_n: int = 5, min_len : int = 3, stemming : bool = False, **kwargs) -> List[Tuple]:
-        doc_embedding = []
-        candidate_embedding = []
-
-        if stemming:
-            stemmer = PorterStemmer()
-            doc_embedding = self.model.embed(stemmer.stem(doc))
-            candidate_embedding = [self.model.embed(stemmer.stem(candidate)) for candidate in candidate_list]
-        
-        else:
-            doc_embedding = self.model.embed(doc)
-            candidate_embedding = [self.model.embed(candidate.lower()) for candidate in candidate_list]
-        
-        doc_sim = []
-        if "MMR" not in kwargs:
-            doc_sim = np.absolute(cosine_similarity(candidate_embedding, doc_embedding.reshape(1, -1)))
-        else:
-            n = len(candidate_list) if len(candidate_list) < top_n else top_n
-            doc_sim = mmr(doc_embedding.reshape(1, -1), candidate_embedding, candidate_list, n, kwargs["MMR"])
-
-        candidate_score = sorted([(candidate_list[i], doc_sim[i][0]) for i in range(len(doc_sim))], reverse= True, key= lambda x: x[1])
-
-        return candidate_score[:top_n]
-
     def extract_kp_from_doc(self, doc, top_n, min_len, stemming, **kwargs) -> Tuple[List[Tuple], List[str]]:
         """
         Concrete method that extracts key-phrases from a given document, with optional arguments
         relevant to its specific functionality
         """
-        doc = Document(doc, self.tagger, self.grammar)
-        doc.pos_tag()
-        doc.extract_candidates()
-        
-        #top_n = self.top_n_candidates(doc, candidate_list, top_n, min_len, stemming, **kwargs)
-        #return (top_n, candidate_list)
+        doc = Document(doc)
+        doc.pos_tag(self.tagger)
+        doc.extract_candidates(min_len, self.grammar)
+        top_n, candidate_set = doc.top_n_candidates(self.model, top_n, min_len, stemming, **kwargs)
+
+        return (top_n, candidate_set)
 
     def extract_kp_from_corpus(self, corpus, top_n=5, min_len=0, stemming=True, **kwargs) -> List[List[Tuple]]:
         """
@@ -78,8 +54,10 @@ class EmbedRank(BaseKPModel):
 
 
 dataset_obj = DataSet(["PubMed"])
-model = EmbedRank("xlm-r-bert-base-nli-stsb-mean-tokens")
+model = EmbedRank("paraphrase-MiniLM-L6-v2")
 res = {}
 
 for dataset in dataset_obj.dataset_content:
    res[dataset] = model.extract_kp_from_corpus(dataset_obj.dataset_content[dataset][0:1], 7, 3, False, MMR = 0.5)
+
+evaluate_kp_extraction(extract_res_labels(res), extract_dataset_labels(dataset_obj.dataset_content), model.name, False)
--- models/pre_processing/document_abstraction.py
+++ models/pre_processing/document_abstraction.py
@@ -1,13 +1,18 @@
 import numpy as np
 
 from nltk import RegexpParser
+from nltk.stem import PorterStemmer
+from sklearn.metrics.pairwise import cosine_similarity
+from typing import List, Tuple, Set
+
+from keybert.mmr import mmr
 
 class Document:
     """
     Simple abstract class to encapsulate a documents representation
     """
 
-    def __init__(self, raw_text, tagger, grammar):
+    def __init__(self, raw_text):
         """
         Stores the raw text representation of the doc, a pos_tagger and the grammar to
         extract candidates with
@@ -15,22 +20,21 @@ class Document:
         self.raw_text = raw_text
         self.doc_sents = []
 
-        self.tagger = tagger 
-        self.grammar = grammar
 
-    def pos_tag(self):
+    def pos_tag(self, tagger):
         """
         Method that handles POS_tagging of an entire document, whilst storing it seperated by sentences
         """
-        self.tagged_text, self.doc_sents = self.tagger.pos_tag_text_sents(self.raw_text)
+        self.tagged_text, self.doc_sents = tagger.pos_tag_text_sents(self.raw_text)
 
-    def extract_candidates(self, min_len : int = 5):
+    def extract_candidates(self, min_len : int = 5, grammar : str = ""):
         """
-        Method that uses Regex patterns on POS tags to extract unique candidates from a tagged document
+        Method that uses Regex patterns on POS tags to extract unique candidates from a tagged document and 
+        stores the sentences each candidate occurs in
         """
         candidate_sents = {}
 
-        parser = RegexpParser(self.grammar)
+        parser = RegexpParser(grammar)
         np_trees = list(parser.parse_sents(self.tagged_text))
 
         for i in range(len(np_trees)):
@@ -43,5 +47,29 @@ class Document:
                     else:
                         candidate_sents[candidate].append(i)
 
-        self.candidate_set = set(candidate_sents.keys())
+        self.candidate_set = list(candidate_sents.keys())
         self.candidate_sents = candidate_sents
+
+    def top_n_candidates(self, model, top_n: int = 5, min_len : int = 3, stemming : bool = False, **kwargs) -> List[Tuple]:
+        doc_embedding = []
+        candidate_embedding = []
+
+        if stemming:
+            stemmer = PorterStemmer()
+            doc_embedding = model.embed(stemmer.stem(self.raw_text))
+            candidate_embedding = [model.embed(stemmer.stem(candidate)) for candidate in self.candidate_set]
+        
+        else:
+            doc_embedding = model.embed(self.raw_text)
+            candidate_embedding = [model.embed(candidate.lower()) for candidate in self.candidate_set]
+        
+        doc_sim = []
+        if "MMR" not in kwargs:
+            doc_sim = np.absolute(cosine_similarity(candidate_embedding, doc_embedding.reshape(1, -1)))
+        else:
+            n = len(self.candidate_set) if len(self.candidate_set) < top_n else top_n
+            doc_sim = mmr(doc_embedding.reshape(1, -1), candidate_embedding, self.candidate_set, n, kwargs["MMR"])
+
+        candidate_score = sorted([(self.candidate_set[i], doc_sim[i][0]) for i in range(len(doc_sim))], reverse= True, key= lambda x: x[1])
+
+        return candidate_score[:top_n], self.candidate_set
