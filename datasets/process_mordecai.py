eaa16ebf067e4411ef8dc3f167731e84cfde7aff
araag2 <artur.guimas@gmail.com>
2022-04-04 10:52:56 +0100

    small change

--- datasets/process_mordecai.py
+++ datasets/process_mordecai.py
@@ -4,7 +4,7 @@ from mordecai import Geoparser
 def process_MKDUC01():
     parser = Geoparser()
 
-    with open(f'..\\raw_data\\MKDUC-01\\MKDUC01.json', 'r') as s_json:
+    with open('..\\raw_data\\MKDUC-01\\MKDUC01.json', 'r') as s_json:
         docs = json.load(s_json)
 
         print(docs)
