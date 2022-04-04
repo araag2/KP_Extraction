774befade0ba8411478c581123bea340cbe7f6fc
araag2 <artur.guimas@gmail.com>
2022-04-04 11:43:33 +0100

    small mordecai adjustment

--- datasets/process_mordecai.py
+++ datasets/process_mordecai.py
@@ -8,3 +8,6 @@ def process_MKDUC01():
         docs = json.load(s_json)
 
         print(docs)
+
+        with open('..\\raw_data\\MKDUC-01\\MKDUC01-Mordecai.json', 'w') as d_json:
+            json.dump(docs, d_json, indent=4, separators=(',', ': '))
