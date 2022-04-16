b05fb22ace2ff0ce79076ffb5a2b5dace73e25db
araag2 <artur.guimas@gmail.com>
2022-04-16 16:34:10 +0100

    New mordecai datapoint info

--- datasets/process_mordecai.py
+++ datasets/process_mordecai.py
@@ -21,7 +21,7 @@ def process_MKDUC01():
 def build_map():
 	with open('C:\\Users\\artur\\Desktop\\stuff\\IST\\Thesis\\Code\\KP_Extraction\\datasets\\raw_data\\MKDUC-01\\MKDUC01-mordecai.json', 'r') as s_json:
 		source = json.load(s_json)
-		res = {"geo_loc" : [], "country_loc" : []}
+		res = {"geo_loc" : [], "country_loc" : [], "full_p" : []}
 		geo_locations = []
 		country_locations = []
 
@@ -31,11 +31,27 @@ def build_map():
 				for entry in data_list:
 					if "geo" in entry:
 						res["geo_loc"].append((float(entry["geo"]["lat"]), float(entry["geo"]["lon"])))
+
+						doc_f = {}
+						for e in entry:
+							if e in ["word", "country_predicted", "country_conf"]:
+								doc_f[e] = entry[e]
+						for e in entry["geo"]:
+							if e in ["country_code3", "geonameid", "place_name"]:
+								doc_f[e] = entry["geo"][e]
+							elif e in ["lat", "lon"]:
+								doc_f[e] = float(entry["geo"][e])
+						res["full_p"].append(doc_f)
 					else:
 						res["country_loc"].append(entry["country_predicted"])
 
 		with open('C:\\Users\\artur\\Desktop\\stuff\\IST\\Thesis\\Code\\KP_Extraction\\datasets\\raw_data\\MKDUC-01\\MKDUC01-geo_locations.json', 'w') as d_json:
 			json.dump(res, d_json, indent=4, separators=(',', ': '))
+
+		px.set_mapbox_access_token("pk.eyJ1IjoiYXJhYWcyIiwiYSI6ImNsMjF6aHo3bDBndWMzYm1xMDlxZG80NWYifQ.EntsjZjhOld8owIpZ8GJmA")	
+		fig = px.scatter_mapbox(res["full_p"], lat="lat", lon="lon", hover_name = "word", hover_data = ["word", "country_predicted", "country_conf", "country_code3", "geonameid", "place_name"], size_max=15, zoom=1, width=1000, height=800)
+		#fig.data[0].marker = dict(size = 5, color="red")
+		fig.show()
 
 #process_MKDUC01()
 build_map()
